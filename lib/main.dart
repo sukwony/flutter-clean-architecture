220cc4e82f475cdab572c2a529ef32249fe3a6fd
Sukwon Yi <sukwon.yi@phloxcorp.io>
2023-02-26 12:12:51 +0900

    Add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,13 @@ class _TabScaffoldExampleState extends State<TabScaffoldExample> {
               ),
               child: Center(
                 child: CupertinoButton(
-                  child: const Text('Next page'),
+                  child: const Text(
+                    'Next page',
+                    style: TextStyle(
+                      fontFamily: 'NotoSansKR',
+                      fontWeight: FontWeight.w200
+                    )
+                    ),
                   onPressed: () {
                     Navigator.of(context).push(
                       CupertinoPageRoute<void>(
