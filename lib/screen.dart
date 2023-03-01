2712b69c3d10a9001a1591e4270d130ca3fae95c
Sukwon Yi <sukwon.yi@phloxcorp.io>
2023-03-02 08:00:51 +0900

    experiment basic navigation

--- lib/screen.dart
+++ lib/screen.dart
@@ -9,11 +9,35 @@ class Screen with _$Screen {
 
   const factory Screen.explore() = Explore;
   const factory Screen.user() = User;
+  const factory Screen.unknown() = Unknown;
+
+  static const explore_ = Screen.explore();
+  static const user_ = Screen.user();
+  static const unknown_ = Screen.unknown();
+
+  static Screen fromPath(String path) {
+    if (path.startsWith('explore')) return explore_;
+    if (path.startsWith('users')) return user_;
+    return unknown_;
+  }
+
+  Map<String, dynamic> parsePath(String path) {
+    final splitted = path.split('/');
+
+    return when(
+      explore: () => {},
+      user: () {
+        return { 'userId': splitted[1] };
+      },
+      unknown: () => {}
+    );
+  }
 
   String get className {
     return when(
       explore: () => 'Explore',
-      user: () => 'User'
+      user: () => 'User',
+      unknown: () => 'Unknown'
     );
   }
 
@@ -21,10 +45,8 @@ class Screen with _$Screen {
     final args = arguments as Map<String, dynamic>;
     return when(
       explore: () => 'explore',
-      user: () => 'users/${args['userId']}'
+      user: () => 'users/${args['userId']}',
+      unknown: () => 'unknown'
     );
   }
-
-  static const explore_ = Screen.explore();
-  static const user_ = Screen.user();
 }
