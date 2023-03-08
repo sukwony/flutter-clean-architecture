e43b2a1ee88537a7e0d71c6d21dfd3959f72874d
Sukwon Yi <sukwon.yi@phloxcorp.io>
2023-03-09 08:37:45 +0900

    scaffold tag screen

--- lib/screen.dart
+++ lib/screen.dart
@@ -8,15 +8,18 @@ class Screen with _$Screen {
   const Screen._();
 
   const factory Screen.explore() = Explore;
+  const factory Screen.tag() = Tag;
   const factory Screen.user() = User;
   const factory Screen.unknown() = Unknown;
 
   static const _explore = Screen.explore();
+  static const _tag = Screen.tag();
   static const _user = Screen.user();
   static const _unknown = Screen.unknown();
 
   static Screen fromPath(String path) {
     if (path.startsWith('explore')) return _explore;
+    if (path.startsWith('tags/')) return _tag;
     if (path.startsWith('users/')) return _user;
     return _unknown;
   }
@@ -26,6 +29,7 @@ class Screen with _$Screen {
 
     return when(
       explore: () => {},
+      tag: () => { 'tagId': splitted[1] },
       user: () {
         return { 'userId': splitted[1] };
       },
@@ -36,6 +40,7 @@ class Screen with _$Screen {
   String get className {
     return when(
       explore: () => 'Explore',
+      tag: () => 'Tag',
       user: () => 'User',
       unknown: () => 'Unknown'
     );
@@ -44,6 +49,7 @@ class Screen with _$Screen {
   String getName(Map<String, dynamic> arguments) {
     return when(
       explore: () => 'explore',
+      tag: () => 'tags/${arguments['tagId']}',
       user: () => 'users/${arguments['userId']}',
       unknown: () => 'unknown'
     );
